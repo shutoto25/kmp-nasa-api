ae071328e33200fab088f4d7d37eb52e541230b9
shuto mohri <1go1e.ices2@gmail.com>
2025-05-10 11:19:05 +0900

    Gradle設定にKoin Composeライブラリを追加

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     //DI
     implementation(libs.koin.core)
     implementation(libs.koin.android)
+    implementation(libs.koin.compose)
     //Navigation
     implementation(libs.voyager.navigator)
     //WorkManager
